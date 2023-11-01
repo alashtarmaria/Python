6641430b08db2731840d8e2e34f454fe31aac960
alashtarmaria <97789031+alashtarmaria@users.noreply.github.com>
2023-11-01 15:45:05 +0300

    Python eğitim

---
+++ List/id_ve_is_yontemi.py
@@ -0,0 +1,21 @@
+# id yöntemi
+
+# Python'da tüm nesneler bir id ile tutulur.
+# Nesnenin bellekteki adresi.
+# Id değeri -> id() fonksiyonu ile bulunur.
+liste = ['a', 'b', 'c', 'd', 'e', 'f']
+yeni_liste = liste[:]
+baska_liste = liste[:]
+print("liste:", liste)
+print("yeni_liste:", yeni_liste)
+print("baska_liste:", baska_liste)
+# liste değişkeninin id'sini alalım
+print(id(liste))
+
+
+# yeni_liste'nin id'si
+print(id(yeni_liste))
+
+
+# baska_liste'nin id'si
+print(id(baska_liste))
