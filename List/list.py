c921448431da27d4a81c086f979e71bd59d64e2c
alashtarmaria <97789031+alashtarmaria@users.noreply.github.com>
2023-11-01 15:04:30 +0300

    Python eğitimi

---
+++ List/list.py
@@ -0,0 +1,26 @@
+# List
+# List bir Dizidir
+# Daha önce, String'lerde dizi tipinden bahsetmiştik. List de bir Dizi tipidir.
+# List'lerde index yapısı ile içindekilere erişilebilir.
+# List'ler [] ile gösterilir.
+
+listem = [10, 20, 30, 40]
+print(listem)
+
+ucgenler = ['eşkanar', 'ikizkenar', 'çeşitkenar']
+print(ucgenler)
+
+karma_liste = ['mail', 2.0, 5, True, False]
+print(karma_liste)
+
+
+bos = []
+print(bos)
+print(type(bos))
+
+
+ic_ice_liste = ['sehir', ['ankara', 'istanbul'], [6, 34]]
+print(ic_ice_liste)
+print(ic_ice_liste[0])
+print(ic_ice_liste[1])
+print(ic_ice_liste[1][0])
