88b9aefcb1f945c917ee3f0430c96d79f7ce4e63
alashtarmaria <97789031+alashtarmaria@users.noreply.github.com>
2023-11-03 15:22:53 +0300

    proje 2

--- Projeler/Proje_2_Kelimeler/proje_2_kelimeler_3.py
+++ Projeler/Proje_2_Kelimeler/proje_2_kelimeler_3.py
@@ -1,21 +1,30 @@
+# Alıştırma 3:
+
+# yasak_harf_var_mi adında bir fonksiyon yaratalım.
+# Bu fonksiyon parametre olarak, bir metin ve yasak harfleri içeren bir string alacak.
+# Eğer metin içerisinde, bu yasak harflerden herhangi biri varsa, True, hiçbiri yoksa False dönecek.
+
 # dosyayı okuyalım
 
 file = open(r'C:\Users\Marya\Desktop\python\Projeler\Proje_2_Kelimeler\kelimeler.txt')
 
-# Şimdi içinde 'ae' harflerinin beraber bulunduğu kelimeleri bulalım:
-for line in file :
-
-    char = "ae"
-
-    for satir in file :
-
-        kelime_dizi = satir.split()
-
-        kelime = kelime_dizi[0]
-
-        if char in kelime:
-            print(kelime)
+def yasak_harf_var_mi(metin, yasak_harfler):
+    
+    for harf in metin:
+        if harf in yasak_harfler:
+            return True
+    else:
+        return False
 
 
 
+cumle = 'Bu bir normal metindir.'
+yasak = 'ae'
+print(yasak_harf_var_mi(cumle, yasak))
+# True    
+
+cumle = 'Bu bir normal metindir.'
+yasak = 'üö'
+print(yasak_harf_var_mi(cumle, yasak))
+# False
     
