51abf298fd0018e90453afdcecf0a767ce2a91e5
alashtarmaria <97789031+alashtarmaria@users.noreply.github.com>
2023-11-05 22:27:16 +0300

    Set

---
+++ Set/set_metodlari_7.py
@@ -0,0 +1,9 @@
+# isdisjoint()
+# Return True if no items in set x is present in set y:
+# 1.set tamamen farklı 2.setten
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "facebook"}
+z = x.isdisjoint(y)
+
+print(z)
---
+++ Set/set_metodlari_8.py
@@ -0,0 +1,7 @@
+# difference_update() metodu bir kümeyi farkları olarak update eder.
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.difference_update(y)
+print("x: ",x)
