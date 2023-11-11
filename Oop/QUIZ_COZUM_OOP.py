717f016dce2f9618f3310ae9335860f026558f23
alashtarmaria <97789031+alashtarmaria@users.noreply.github.com>
2023-11-11 20:12:40 +0300

    Quiz çözümleri

---
+++ Oop/QUIZ_COZUM_OOP.py
@@ -0,0 +1,95 @@
+"""
+ OOP ile ilgili QUIZ - 10 Soru
+"""
+
+# --------------------------------------------------------------------------------------#
+
+# Soru 1:
+"""
+Adı Ogrenci olan bir class tanımlayın.
+Ogrenci'nin 2 özelliği var:
+* isim (str)
+* numara (str)
+
+Bu Ogrenci class'ından iki öğrenci yaratın.
+Bilgileri şu şekilde olacak:
+1- isim: James Bond, numara: 007
+2- isim: Klark Kent, numara: 333
+
+Sonra bu iki öğrencinin adlarını print edin.
+
+İpuçları:
+* __init__
+
+Beklenen Sonuç:
+James Bond
+Klark Kent
+"""
+
+# Çözüm 1:
+class Ogrenci:
+    def __init__(self, isim, numara):
+        self.isim = isim
+        self.numara = numara
+
+
+# Öğrenci nesneleri yarat
+ogr_1 = Ogrenci('James Bond', '007')
+ogr_2 = Ogrenci('Klark Kent', '333')
+
+# print(ogr_1.isim)
+# print(ogr_2.isim)
+
+# --------------------------------------------------------------------------------------#
+
+# Soru 2:
+"""
+Soru 1'deki Ogrenci class'ımızın şimdi de kayıt olduğu dersleri tutan,
+bir attribute'u (özelliği) daha olsun. 
+Bu özelliğin adı 'dersler' olsun ve list tipinde olsun.
+Bu özellik, Ogrenci yaratılırken boş liste olarak initialize olacak.
+
+Ogrenci derslere birer birer katılacak.
+Dolayısı ile 'derse_katil()' adında bir metodu da olacak.
+Be metod parametre olarak katılacak dersi alacak.
+Eğer ders henüz listede yoksa, o zaman kayıt olacak.
+
+Ek olarak 'get_dersler' adında bir metod daha olacak.
+Bu metod bize öğrencinin kayıt olduğu ders listesini verecek.
+
+Buna göre Ogrenci class'ını modifiye edin.
+
+Öğrencinin bilgileri şu şekilde olacak:
+isim: Cin Ali, numara: 1111
+katılacağı dersler: Temel Python ve Yapay Zeka
+
+Beklenen Sonuç:
+ogrenci.get_dersler()  ->  ['Temel Python', 'Yapay Zeka']
+"""
+
+# Çözüm 2:
+class Ogrenci:
+    def __init__(self, isim, numara):
+        self.isim = isim
+        self.numara = numara
+        # ders listesini -> []
+        self.dersler = []
+
+    def derse_katil(self, ders):
+        # bu ders yoksa -> ekle
+        if not ders in self.dersler:
+            self.dersler.append(ders)
+
+    def get_dersler(self):
+        return self.dersler
+
+
+ogrenci = Ogrenci('Cin Ali', '1111')
+
+ogrenci.derse_katil('Temel Python')
+ogrenci.derse_katil('Yapay Zeka')
+
+# print(ogrenci.get_dersler())
+
+# --------------------------------------------------------------------------------------#
+
